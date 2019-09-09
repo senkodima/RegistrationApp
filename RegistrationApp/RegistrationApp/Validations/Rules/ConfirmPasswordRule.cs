cdce84b7aa77a7cfec9647bff479b4207ec9c8af
Dmitry Senko <senkodima22@gmail.com>
2019-09-09 22:43:53 +0300

    add ConfirmPassword Entry + ConfirmPasswordRule

---
+++ RegistrationApp/RegistrationApp/Validations/Rules/ConfirmPasswordRule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RegistrationApp.Validations.Rules
+{
+    public class ConfirmPasswordRule : IValidationRule<string>
+    {
+        public string ValidationMessage { get; set; }
+
+        private Func<string> _confirmMethod;
+
+        public ConfirmPasswordRule(Func<string> getPassword)
+        {
+            ValidationMessage = "Passwords don't match";
+            _confirmMethod = getPassword;
+        }
+
+        delegate ValidatableObject<string> ValuePass();
+
+        public bool Check(string value)
+        {
+            return string.Equals(_confirmMethod?.Invoke(), value);
+        }
+    }
+}
