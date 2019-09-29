ca6d244ac7b3a2a6d4e0462702cef7c312868585
Dmitry Senko <senkodima22@gmail.com>
2019-09-30 00:22:22 +0300

    Create local database to save users and user tasks, add validation to email uniqueness

---
+++ RegistrationApp/RegistrationApp/Autorisation/AutorisationService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RegistrationApp.Models;
+using Xamarin.Forms;
+
+namespace RegistrationApp.Autorisation
+{
+    public static class AutorisationService
+    {
+        private static User _currentUser;
+
+        public static bool CheckEmailUniqueness(string value)
+        {
+            var users = App.UserDatabase.GetAllUsersAsync().Result;
+            return (users.Where(u => u.Email == value)
+                        .FirstOrDefault() == null);
+        }
+
+        public static async void SignUpUserAsync(User user)
+        {
+            if (CheckEmailUniqueness(user.Email))
+            {
+                await App.UserDatabase.SaveUserAsync(user);
+                await Application.Current.MainPage.Navigation.PopAsync();
+            }
+            else
+            {
+                Application.Current.MainPage.DisplayAlert("ERROR", "Email already exists", "OK");
+            }
+        }
+
+        
+    }
+}
